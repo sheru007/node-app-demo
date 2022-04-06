a51c88c71975704e373751b39e395f3265d5daa1
sheru007 <jnvh1233sheru@gmail.com>
2022-04-06 11:54:13 +0530

    api updated

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
-
+const PORT = 5000
+app.use(
+    express.urlencoded({
+      extended: true
+    })
+  )
+  
+app.use(express.json())
 app.get('/', (req, res) => {
-    res.json({name: "sheru khan", message: "hii this is BFF server"})
+    res.json({ name: "sheru-khan-bff", message: "hii this is BFF server" })
 })
 
 app.get('/api', (req, res) => {
-    res.json({name: "sheru khan", message: "hii this is BFF server with /api path"})
+    res.json({ name: "sheru-khan-bff-api", message: "hii this is BFF server with /api path" })
+})
+app.post('/api/update-title', (req, res) => {
+    console.log({header: req.headers, body: req.body})
+    const bearerHeader = req.headers['authorization'];
+    const {title = 'no-title'} = req.body
+
+    console.log({bearerHeader})
+    if(!bearerHeader ){
+        return res.sendStatus(403);
+    }
+    const bearerToken = bearerHeader.split(' ')[1];
+    console.log({bearerToken})
+    if(bearerToken !== 'sk007') {
+        return res.json({message: 'token is wrong'})
+    }
+        
+    res.json({ name: "sheru-khan-bff-update-title-api", message: "hii this is BFF server with /api path", title })
 })
 
 app.get('/api/user', (req, res) => {
-    res.json({name: "sheru khan", message: "hii this is BFF server with /api/user path"})
+    res.json({ name: "sheru-khan-bff-api-user", message: "hii this is BFF server with /api/user path" })
 })
 
 app.listen(PORT, () => {
