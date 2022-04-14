7f8afa61e9dba8ba4dc0e9abfe77de91f81f5f7f
sheru007 <jnvh1233sheru@gmail.com>
2022-04-14 10:27:34 +0530

    304 nad csrf done

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
-const app = express()
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf')
+
+
+// constants
 const PORT = 5000
-app.use(
-    express.urlencoded({
-      extended: true
-    })
-  )
-  
+const csrfProtection = csrf({cookie: true})
+const app = express()
+
+// middle ware
+app.use(cookieParser())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+// API
 app.get('/', (req, res) => {
     res.json({ name: "sheru-khan-bff", message: "hii this is BFF server" })
 })
@@ -34,9 +41,42 @@ app.post('/api/update-title', (req, res) => {
 })
 
 app.get('/api/user', (req, res) => {
-    res.json({ name: "sheru-khan-bff-api-user", message: "hii this is BFF server with /api/user path" })
+    res.set('Cache-Control', 'no-store')
+    res.status(200).json({ name: "sheru-khan-bff-api-user", message: "hii this is BFF server with /api/user path" })
 })
 
+app.get("/api/csrf-poc",csrfProtection, (req, res) => {
+
+    res.send(`
+    <html>
+    <form id="myForm" action="/api/csrf-poc" method="POST" target="_self">
+    Account:<input type="text" name="account" value="your friend"/><br/>
+    Amount:<input type="text" name="amount" value="$5000"/>
+    <input type="hidden" name="_csrf" value="${req.csrfToken()}"/>
+      <button type="submit">Transfer Money</button>
+    </form>
+    </html>
+    `)
+});
+
+app.post("/api/csrf-poc",csrfProtection, (req, res) => {
+    console.log({cookie: req.cookies, body: req.body})
+    if (isAuthenticated(req.cookies["session"])) {
+        // Transfer money and insert data in the database
+        console.info("Transferring Money...")
+        res.send("OK - Money Transfer done")
+    } else {
+        res.status(400).send("Bad Request")
+    }
+});
+
 app.listen(PORT, () => {
-    console.log('node server started !!!!')
+    console.log(`node bff-server started !!!! at ${PORT}`)
 })
+
+
+const isAuthenticated = (session) => {
+    // We should check session in a store or something like that
+    console.log({session})
+    return true || (session === "valid_user")
+}
