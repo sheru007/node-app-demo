55d5934bbbabbc073853a249ccc719c267f1c831
sheru007 <jnvh1233sheru@gmail.com>
2022-04-22 18:14:07 +0530

    csruff package console done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cookieParser = require('cookie-parser');
-const csrf = require('csurf')
+const csrf = require('./csruff')
 const cors = require('cors')
 
 // constants
